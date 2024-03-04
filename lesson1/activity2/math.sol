f598d562f16aae14edd6a0404bde1dad2cd686c7
LarsWeb3 <28land82@gmail.com>
2024-03-04 12:02:45 +0800

    Math.sol modified according to tasks given. Kindly check. Thanks

--- lesson1/activity2/math.sol
+++ lesson1/activity2/math.sol
@@ -27,4 +27,25 @@ contract Math {
     // 2. Declare a function called divide that takes in one uint8 variable
     //    named whatever you wish and return the value of the variable divided
     //    by the value of z
+
+}
+
+contract MathOperations {
+    uint8 public z = 2; // Task 3: Declare a state variable of type uint8 called z with a value of 2
+
+    // Task 1: Declare a function called subtract that takes in a uint8 variable and returns the value of the variable minus 2
+    function subtract(uint8 _value) public pure returns (uint8) {
+        return _value - 2;
+    }
+
+    // Task 2: Declare a function called multiply that takes in two uint8 variables and returns the value of the variables multiplied by each other
+    function multiply(uint8 _value1, uint8 _value2) public pure returns (uint8) {
+        return _value1 * _value2;
+    }
+
+    // Task 3: Declare a function called divide that takes in one uint8 variable and returns the value of the variable divided by the value of z
+    function divide(uint8 _value) public view returns (uint8) {
+        require(z != 0, "Division by zero is not allowed");
+        return _value / z;
+    }
 }
